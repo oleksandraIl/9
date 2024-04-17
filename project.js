3c5aaef52b460ab2a196a04f9a845a7c575e298b
oleksandraIl <144255710+oleksandraIl@users.noreply.github.com>
2024-04-17 14:21:12 +0300

    Update project.js

--- project.js
+++ project.js
@@ -2,8 +2,18 @@ const fs = require('fs');
 const fileName = 'example.txt';
 
 fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err) return console.error('Помилка під час зчитування файлу:', err);
+    if (err) {
+        return console.error('Помилка під час зчитування файлу:', err);
+    }
     
-    const modifiedText = data.split('').map((char, index) => (index % 3 === 2 && /[a-z]/.test(char)) ? char.toUpperCase() : char).join('');
+    const modifiedText = data.split('').map((char, index) => {
+        if (index % 3 === 2 && /[a-z]/.test(char)) {
+            return char.toUpperCase();
+        } else {
+            return char;
+        }
+    }).join('');
 
     console.log('Модифікований текст:', modifiedText);
+});
+
